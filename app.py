df3555d6f2640f194a4d01838970d468c3115d72
venkatasai-03 <141182370+venkatasai-03@users.noreply.github.com>
2024-11-19 21:11:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -77,5 +77,4 @@ def index():
 
 # Run the Flask app
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=5000)
